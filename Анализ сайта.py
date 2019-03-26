223424b4e8df8de00b8910944f41751b44679874
Antonkorzy <Antonkorzy@gmail.com>
2019-03-26 21:30:16 +0500

    Update Анализ сайта.py

--- Анализ сайта.py
+++ Анализ сайта.py
@@ -1,29 +1,129 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Dec 14 10:54:49 2018
-
-@author: Anton
-"""
-
 import requests
 from bs4 import BeautifulSoup as bs
+import pymongo
+import  re
 
-url = r"https://sp.booking.com/searchresults.ru.html?aid=1535069&label=ruru-edge-ntp-topsites&sid=56850882b961a5e91c2feeb032e73d17&ac_click_type=b&ac_position=0&checkin_month=12&checkin_monthday=8&checkin_year=2018&checkout_month=12&checkout_monthday=9&checkout_year=2018&class_interval=1&dest_id=-3023495&dest_type=city&dtdisc=0&from_sf=1&group_adults=2&group_children=0&iata=TJM&inac=0&index_postcard=0&label_click=undef&no_rooms=1&postcard=0&raw_dest_type=city&room1=A%2CA&sb_price_type=total&search_selected=1&shw_aparth=1&slp_r_match=0&src=index&src_elem=sb&srpvid=80ac25a446fd0079&ss=%D0%A2%D1%8E%D0%BC%D0%B5%D0%BD%D1%8C%2C%20%D0%A2%D1%8E%D0%BC%D0%B5%D0%BD%D1%81%D0%BA%D0%B0%D1%8F%20%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C%2C%20%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D1%8F&ss_all=0&ss_raw=%D1%82%D1%8E%D0%BC%D0%B5%D0%BD%D1%8C&ssb=empty&sshis=0&rows=40&offset=40"
+headers = {'accept': '*/*',
+           'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'}
+db_address = 'localhost'
+db_port = 27017
+db_name = 'hh_parse_data'
+coll_name = 'hhru'
 
-while True:
-    headers = {'User-Agent': 
-            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36' 
-    } 
-    r = requests.get(url, headers=headers) 
-    soup = bs(r.content,"html.parser")
-    hotelNames=soup.find_all("span", {"class":"sr-hotel__name "})
 
-    for hotelName in hotelNames: 
-            print(hotelName.get_text().strip())
+def hhru_parse(base_url, header, search_parr):
+    jobs_loc = []
+    session = requests.Session()
+    request = session.get(base_url, headers=header)
+    if request.status_code == 200:
+        while True:
+            soup = bs(request.content, 'html.parser')
+            divs = soup.find_all('div', attrs={'data-qa': 'vacancy-serp__vacancy'})
+            for div in divs:
+                title = div.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-title'}).text
+                compensation = div.find('div', attrs={'data-qa': 'vacancy-serp__vacancy-compensation'})
+                if compensation == None:  # Если зарплата не указана
+                    compensation = ''
+                else:
+                    compensation = div.find('div', attrs={'data-qa': 'vacancy-serp__vacancy-compensation'}).text
+                href = div.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-title'})['href']
+                company = div.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-employer'}).text
+                text1 = div.find('div', attrs={'data-qa': 'vacancy-serp__vacancy_snippet_responsibility'}).text
+                text2 = div.find('div', attrs={'data-qa': 'vacancy-serp__vacancy_snippet_requirement'}).text
+                content = text1 + '  ' + text2
+                all_txt = [title, compensation, company, content, href, search_parr]
+                jobs_loc.append(all_txt)
 
-    nextPage = soup.find("a", title="Next page")
-    if nextPage !=None:
-        url ="https://www.booking.com/" + nextPage.attrs["href"]
-        print("Обработка следующей страницы.....")
+            nextPage = soup.find("a", {"data-qa": "pager-next"})
+            #print(nextPage)
+            if nextPage is not None:
+                base_url = url_base + nextPage.attrs["href"]
+                request = session.get(base_url, headers=header)
+            else:
+                break
+        return jobs_loc
     else:
-        break
+        print('error')
+
+
+def hhru_write_db(connect, port, db, coll, job_list):
+    conn = pymongo.MongoClient(connect, port)
+    db = conn[db]
+    coll = db[coll]
+    coll.remove({"_search_parr": job_list[0][5]})
+    for i in job_list:
+        doc = {"_job_name": i[0],
+               "_salary": i[1],
+               "_company": i[2],
+               "_description": i[3],
+               "_link": i[4],
+               "_search_parr": i[5]
+               }
+        coll.save(doc)
+
+def salary_calc_avg(connect, port, db, coll,search_value):
+    conn = pymongo.MongoClient(connect, port)
+    db = conn[db]
+    coll = db[coll]
+    avg_salary = 0
+    count = 0
+
+    for item in coll.find({"_search_parr": search_value}):
+       # print(re.sub("[^-0-9]", "", item["_salary"]).split('-'))
+        avg_el = re.sub("[^-0-9]", "", item["_salary"]).split('-')
+        if len(avg_el) == 2:
+            avg_salary = (int(avg_el[0] or 0) + int(avg_el[1] or 0))/2 + int(avg_salary)
+        else:
+            avg_salary = int(avg_el[0] or 0) + int(avg_salary)
+        if int(re.sub("[^0-9]", "", item["_salary"]) or 0) > 0:
+            count = count + 1
+   # print(avg_salary/count)
+    #print(count)
+    if count == 0:
+        return 0
+    else:
+        return avg_salary/count
+
+exit_flag = False
+flag_country = False
+flag_prof = False
+flag_period = False
+
+print('Вас приветствует программа для подсчёта среднего уровня зарплаты для введенной должности')
+while (not exit_flag):
+    if not flag_country:
+        print('Введите номер региона вашего проживания (Пермский край - 1317):')
+        search_area = input() #'1317'
+    if not flag_prof:
+        print('Введите наименование должности (или ключевое слово для поиска набора профессий):')
+        search_value = input()# 'python'
+    if not flag_period:
+        print('Введите период, за который хотите получить информацию (одно число):')
+        search_period = input() #'30'
+    url_base = 'https://perm.hh.ru/'
+    url = url_base + 'search/vacancy?area=' + search_area + '&clusters=true&enable_snippets=true&search_period=' + search_period + '&text=' + search_value + '&page=0'  # area=1317 - Пермский край, search_period=30 - период просмотра, page=0 - Номер страницы
+    jobs = []
+
+    jobs = hhru_parse(url, headers, search_value)
+    print('Данные успешно скачаны')
+    hhru_write_db(db_address, db_port, db_name, coll_name, jobs)
+    print('Данные успешно обновлены в БД')
+    print('Средняя заработная плата по вашему поисковому запросу: ' + str(salary_calc_avg(db_address, db_port, db_name, coll_name, search_value)))
+    print('Вы хотите выполнить поиск по другой профессии? Y/N')
+    q1 = input()
+    if q1 == 'Y':
+        flag_country = True
+        flag_period = True
+    else:
+        print('Вы хотите выполнить поиск по всем новым параметрам? Y/N')
+        q2 = input()
+        if q2 == 'Y':
+            flag_country = False
+            flag_country = False
+        else:
+            exit_flag = True
+    print('Работа с программой завершена')
+
+
+
+
